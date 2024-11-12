ec545b3d3dacdc0792ee8fbc24877eff0268ab71
AndyZzzZzzZzz <zkpAndy@outlook.com>
2024-11-12 01:08:39 -0800

    Update source code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,8 @@
 import React from "react";
 import {
   AboutSection,
-  ArticlesSection,
   ContactSection,
   HeroSection,
-  InterestsSection,
   Page,
   ProjectsSection,
   Seo,
@@ -16,10 +14,8 @@ export default function IndexPage() {
       <Seo title="Gatsby Starter for Portfolio Minimal" />
       <Page useSplashScreenAnimation>
         <HeroSection sectionId="hero" />
-        <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />
         <AboutSection sectionId="about" heading="About Portfolio Minimal" />
-        <InterestsSection sectionId="details" heading="Details" />
-        <ProjectsSection sectionId="features" heading="Built-in Features" />
+        <ProjectsSection sectionId="features" heading="Project Gallery" />
         <ContactSection sectionId="github" heading="Issues?" />
       </Page>
     </>
